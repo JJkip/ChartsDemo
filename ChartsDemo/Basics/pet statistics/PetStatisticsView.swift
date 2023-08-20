1b09fd7af55f14984b7871c691e9ff0b1e8fad55
jjkip <jjlangatjose@gmail.com>
2023-08-20 09:36:19 +0300

    Working with Multiple Data Series in SwiftUI

--- ChartsDemo/Basics/pet statistics/PetStatisticsView.swift
+++ ChartsDemo/Basics/pet statistics/PetStatisticsView.swift
@@ -32,8 +32,31 @@ struct PetStatisticsView: View {
     }
 }
 
+struct SecondPetStatisticsView: View {
+    let catData = PetData.catExample
+    let dogData = PetData.dogExamples
+    var data: [(type: String, petData:[PetData])] {
+        [(type:"cat", petData: catData),
+         (type:"dog", petData: dogData)]
+    }
+    var body: some View {
+        Chart(data,id: \.type) { dataSeries in
+            ForEach(dataSeries.petData) { data in
+                LineMark(x: .value("Year", data.year),
+                        y: .value("Population", data.population)
+                )
+            }
+            .foregroundStyle(by: .value("Pet type", dataSeries.type))
+            .symbol(by: .value("Pet type",dataSeries.type))
+        }
+        .chartXScale(domain: 1998...2024)
+        .aspectRatio(1,contentMode: .fit)
+        .padding()
+    }
+}
+
 struct PetStatisticsView_Previews: PreviewProvider {
     static var previews: some View {
-        PetStatisticsView()
+        SecondPetStatisticsView()
     }
 }
